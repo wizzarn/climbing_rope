2ccd900678506533267d9356f32402f1f6263ba8
Victor Daniel Gonzalez Flores <victor.gzz.flores@hotmail.com>
2017-07-12 01:28:59 -0500

    Added size level bar

---
+++ Assets/Scripts/GUI/HealthBarController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour {
+	
+	public static HealthBarController Instance;
+
+	private float MaxLife = 100.0f;
+	[Range(0, 100)]
+	public float actualHealt;
+	private Image imageBar;
+
+	void Awake()
+	{
+		if (Instance != null)
+		{
+			Debug.LogError("Multiple instances of HealthBarController!");
+		}
+		
+		Instance = this;
+	}
+
+	void Start() {
+		actualHealt = MaxLife;
+		imageBar = this.GetComponent<Image> ();
+	}
+ 
+	void Update () {
+		if (!imageBar)
+			return;
+		float pointHealth = actualHealt / MaxLife;
+		this.imageBar.fillAmount = pointHealth;
+	}
+	
+	public void setMaxLife(float newMaxLife) {
+		this.MaxLife = Mathf.Abs (newMaxLife);
+		this.actualHealt = this.actualHealt < MaxLife ? MaxLife : actualHealt;
+		actualHealt = Mathf.Max (actualHealt, 0);
+
+		if (!imageBar)
+			return;
+		float pointHealth = actualHealt / MaxLife;
+		this.imageBar.fillAmount = pointHealth;
+	}
+
+
+	public void decraseHealth(float decrease) {
+
+		decrease = Mathf.Abs (decrease);
+		actualHealt -= decrease;
+		actualHealt = Mathf.Max (actualHealt, 0);
+
+		if (!imageBar)
+			return;
+		float pointHealth = actualHealt / MaxLife;
+		this.imageBar.fillAmount = pointHealth;
+	}
+}
